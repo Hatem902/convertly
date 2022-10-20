f453b5b2c6a09608c8e90bb5c69713a4915bb98a
Hatem Lamine <hatelm902@gmail.com>
2022-10-20 13:28:39 +0100

    fix : fixed homeComponent routing

--- convertly-frontend/src/app/app-routing.module.ts
+++ convertly-frontend/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeModule } from './home/home.module';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeModule },
+  { path: 'home', component: HomeComponent },
 ];
 
 @NgModule({
--- convertly-frontend/src/app/home/home.module.ts
+++ convertly-frontend/src/app/home/home.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent
+  ],
   imports: [
     CommonModule
   ]
