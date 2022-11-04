75016cea32b39a6179ede716e1eda50dd898a375
Hatem Lamine <hatelm902@gmail.com>
2022-11-04 03:01:15 +0100

    feature : setup theme colors and built a nav-bar

--- convertly-frontend/tailwind.config.js
+++ convertly-frontend/tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "#FFFFFF",
+        red: "#b31b1b",
+        gr: "34 197 94",
+        black: "#000000",
+        gray: "#E0E0E0",
+      },
+      //font families
+      //font-sizes
+    },
   },
   plugins: [],
 };
