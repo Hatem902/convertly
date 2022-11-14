e20631d3f13225f27f6ac7f1dfeef75d23844c03
Hatem Lamine <hatelm902@gmail.com>
2022-11-14 23:23:29 +0100

    feature x3 : designed app logo + added isSelected Indicator for nav-items + setup navbar responsiveness

--- convertly-frontend/src/app/layouts/nav-bar/nav-bar.component.ts
+++ convertly-frontend/src/app/layouts/nav-bar/nav-bar.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss']
+  styleUrls: ['./nav-bar.component.scss'],
 })
 export class NavBarComponent implements OnInit {
+  navItems = [
+    { name: 'Convertly', isMain: true },
+    { name: 'API' },
+    { name: 'Formats' },
+    { name: 'Pricing' },
+  ];
+  selectedItem = { name: 'Convertly' };
+  selectItem = (name: string) => (this.selectedItem.name = name);
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
