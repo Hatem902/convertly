5eec72f6f198c454cf88b96c46d9f0836d6ee0c6
Hatem Lamine <hatelm902@gmail.com>
2022-11-21 18:05:47 +0100

    feature : user can browse local files

--- convertly-frontend/src/app/home/components/hero-section/hero-section.component.ts
+++ convertly-frontend/src/app/home/components/hero-section/hero-section.component.ts
@@ -7,9 +7,26 @@ import { faFileCirclePlus } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./hero-section.component.scss'],
 })
 export class HeroSectionComponent implements OnInit {
-  f = faFileCirclePlus;
+  faFileCirclePlus = faFileCirclePlus;
 
+  file: any;
   constructor() {}
-
+  selectFile() {
+    document.getElementById('file')?.click();
+  }
   ngOnInit(): void {}
+  fileSelected(event: any) {
+    console.log(event.currentTarget.files);
+    this.file = event.currentTarget.files[0];
+  }
+  /* files: any;
+  constructor() {}
+  selectFiles() {
+    document.getElementById('files')?.click();
+  }
+  ngOnInit(): void {}
+  filesSelected(event: any) {
+    console.log(event.currentTarget.files);
+    this.files = event.currentTarget.files[0];
+  } */
 }
