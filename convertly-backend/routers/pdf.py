0a1132c190cd3cac26560bf4045c808198a776cc
idrisskhaled1 <khaled.idriss@elyadata.com>
2022-11-25 11:07:54 +0100

    added pdf routes

--- convertly-backend/routers/pdf.py
+++ convertly-backend/routers/pdf.py
@@ -6,21 +6,21 @@ router = APIRouter()
 @router.post("/pdf/docx", tags=["pdf"])
 async def convert_to_docx(file: UploadFile):
     content = await file.read()
-    open('file.pdf.','wb').write(content)
+    open('uploads/file.pdf.','wb').write(content)
     pdf.convert_pdf2docx('file.pdf','file.docx')
     return {"filename": file.filename}
 
 @router.post("/pdf/extract-text", tags=["pdf"])
 async def extract_text(file: UploadFile):
     content = await file.read()
-    open('file.pdf.','wb').write(content)
+    open('uploads/file.pdf.','wb').write(content)
     result = pdf.extract_text('file.pdf')
     return result
 
 @router.post("/pdf/html", tags=["pdf"])
 async def convert_to_html(file: UploadFile):
     content = await file.read()
-    open('file.pdf.','wb').write(content)
+    open('uploads/file.pdf.','wb').write(content)
     pdf.convert_pdf_to_html('file.pdf')
     return {"filename": file.filename}
 
@@ -28,6 +28,26 @@ async def convert_to_html(file: UploadFile):
 @router.post("/pdf/images", tags=["pdf"])
 async def convert_to_images(file: UploadFile):
     content = await file.read()
-    open('file.pdf.','wb').write(content)
+    open('uploads/file.pdf.','wb').write(content)
     pdf.convert_pdf_to_images('file.pdf')
     return {"filename": file.filename}
+
+
+
+@router.post("/pdf/powerpoint", tags=["pdf"])
+async def convert_to_powerpoint(file: UploadFile):
+    content = await file.read()
+    open('uploads/file.pdf.','wb').write(content)
+    pdf.convert_pdf2pptx('file.pdf')
+    return {"filename": file.filename}
+
+
+
+@router.post("/pdf/excel", tags=["pdf"])
+async def convert_to_excel(file: UploadFile):
+    content = await file.read()
+    open('uploads/file.pdf.','wb').write(content)
+    pdf.convert_pdf2excel('file.pdf')
+    return {"filename": file.filename}
+
+
--- convertly-backend/services/pdf.py
+++ convertly-backend/services/pdf.py
@@ -2,6 +2,7 @@ from pdf2docx import  Converter
 import PyPDF2
 import aspose.words as aw
 from pdf2image import convert_from_path
+import aspose.slides as slides
 
 def convert_pdf_to_html(input_file : str):
     doc = aw.Document(input_file)
@@ -33,7 +34,16 @@ def convert_pdf2docx(input_file: str, output_file: str):
     pdf_file = input_file
     docx_file = output_file
     cv = Converter(pdf_file)
-    cv.convert(docx_file)  # all pages by default
+    cv.convert(docx_file)
     cv.close()
 
 
+def convert_pdf2pptx(input_file : str):
+    with slides.Presentation() as pres:
+        pres.slides.remove_at(0)
+        pres.slides.add_from_pdf(input_file)
+        pres.save("output_file.pptx", slides.export.SaveFormat.PPTX)
+
+def convert_pdf2excel(input_file : str):
+    doc = aw.Document(input_file)
+    doc.save("document.xls")
