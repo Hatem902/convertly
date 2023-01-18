0014c73547ba2e735054bc8b00232502c2784a1d
Garrach <faresgarrach@insat.u-carthage.tn>
2023-01-18 12:40:27 +0100

    fixing file name

--- convertly-backend/routers/docx.py
+++ convertly-backend/routers/docx.py
@@ -6,26 +6,36 @@ router = APIRouter()
 async def convert_to_pdf(file: UploadFile):
     content = await file.read()
     open('uploads/file.docx.','wb').write(content)
-    return docx.convert_docx_to_pdf('uploads/file.docx')
-    
+    res = docx.convert_docx_to_pdf('uploads/file.docx')
+    if "file_name" in res:
+        res["file_name"] = file.filename
+    return res    
 
 @router.post("/docx/image", tags=["docx"])
 async def convert_to_images(file: UploadFile):
     content = await file.read()
     open('uploads/file.docx.','wb').write(content)
-    return docx.convert_docx_to_images('uploads/file.docx')
+    res = docx.convert_docx_to_images('uploads/file.docx')
+    if "file_name" in res:
+        res["file_name"] = file.filename
+    return res  
 
 
 @router.post("/docx/text", tags=["docx"])
 async def convert_to_text(file: UploadFile):
     content = await file.read()
     open('uploads/file.docx.','wb').write(content)
-    return docx.convert_docx_to_text('uploads/file.docx')
+    res = docx.convert_docx_to_text('uploads/file.docx')
+    if "file_name" in res:
+        res["file_name"] = file.filename
+    return res  
      
 
 @router.post("/docx/html", tags=["docx"])
 async def convert_to_html(file: UploadFile):
     content = await file.read()
     open('uploads/file.docx.','wb').write(content)
-    return docx.convert_docx_to_html('uploads/file.docx')
-    
+    res = docx.convert_docx_to_html('uploads/file.docx')
+    if "file_name" in res:
+        res["file_name"] = file.filename
+    return res  
